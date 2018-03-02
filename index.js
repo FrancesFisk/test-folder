58728dc942b2965add2ad58f3976f4353c66167f
Lewis D <lewis.dackam@gmail.com>
2018-03-02 18:42:14 -0500

    added multiplication

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ function addTwoNumbers(num1, num2){
     let result = num1 + num2;
     return result;
 } 
+
+function multiplyTwoNums(num1, num2) {
+  console.log(num1 * num2)
+  return num1 * num2;
+}
+
 addTwoNumbers(1, 2);
+multiplyTwoNums(1,3);
